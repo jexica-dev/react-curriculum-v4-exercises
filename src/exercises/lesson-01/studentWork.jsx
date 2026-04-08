098a13862cca559caa85abbd8513059010a32e16
jexica ayran <jess.ayran@gmail.com>
2026-04-07 21:39:05 -0700

    lesson 01 complete

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,34 @@
 
 export default function StudentWork() {
   //add variables here
+  //    - A variable for your name
+  //  - A variable for your age
+  //  - A list of your hobbies using an array
+
+  const name = 'Jessica Ayran';
+  const age = 33;
+  const hobbies = [
+    'coding',
+    'planting',
+    'being creative',
+    'discovering new musicians',
+    'making art videos',
+    'cooking',
+    'baking',
+  ];
   return (
     <div>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>ABOUT</h1>
+      <p>
+        My name is {name} and I'm {age} years old.
+      </p>
+      <h2>HOBBIES</h2>
+      <ul>
+        {hobbies.map((hobby) => (
+          <li key={hobby}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }

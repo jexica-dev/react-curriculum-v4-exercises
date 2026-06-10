c4633fa94efa0ca9565fef458ed796b5141facd5
jexica ayran <jess.ayran@gmail.com>
2026-06-09 22:07:34 -0700

    lesson 10 complete

--- src/exercises/lesson-10/pages/NotFound.jsx
+++ src/exercises/lesson-10/pages/NotFound.jsx
@@ -1,11 +1,13 @@
 import { Link, useLocation } from 'react-router-dom';
 
 export default function NotFound() {
-  const pathname = '';
+  const pathname = useLocation();
 
   return (
     <section>
       <h2>404: Not Found</h2>
+      <p>Path "{pathname}" does not exist.</p>
+      <Link to="/">Go Home</Link>
     </section>
   );
 }
